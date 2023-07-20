c5c6b8edda3d4c039546565e7628e6acb088e2f3
anderson-nunes <andersonribeironunes@hotmail.com>
2023-07-20 11:34:20 -0300

    Atualizando dados

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,12 +1,17 @@
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import { MainContainer } from "../style";
 
 function ProfilePage() {
 
+  const params = useParams()
+
+  const user = params.name
+
   return (
     <MainContainer>
       <Header />
-      <h1>Página acerca do usuário</h1>
+      <h1>Página acerca do usuário{`${user}`}</h1>
     </MainContainer>
   );
 }
